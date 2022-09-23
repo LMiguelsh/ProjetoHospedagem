3ca53f2bedb96cbcf8c4e2aadd18ddd0b2ae164b
LMiguelsh <110040313+LMiguelsh@users.noreply.github.com>
2022-09-23 09:45:49 -0300

    Add files via upload

---
+++ Models/Reserva.cs
@@ -0,0 +1,60 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            try {
+                if (Suite.Capacidade >= hospedes.Count) {
+                    Hospedes = hospedes;
+                }
+            }
+            catch (Exception) {
+
+                Console.WriteLine("Quantidade de hospedes maior que capacidade da suite.");
+            }
+            
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+            return Hospedes.Count();
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (valor >= 10) {
+                valor = valor * 0.90m;
+            }
+
+            return valor;
+        }
+    }
+}
